fe7a6d85f3df11a177816c49e635ccfe05e0773f
yrras <161315617+yrras@users.noreply.github.com>
2024-05-08 06:05:53 +0900

    課題提出

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+
+
+const addBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+addBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    text.textContent = "ボタンをクリックしました";
+  }, 2000);
+});
