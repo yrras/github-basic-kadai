950906f0a3a4144f485c4945cc9c86996b24799a
yrras <161315617+yrras@users.noreply.github.com>
2024-05-11 16:30:15 +0900

    課題提出

---
+++ kadai_0007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>連想配列課題</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+      $personal_data=[ 'name'=>'onion','price'=>'200','weight'=>'160'];
+      print_r($personal_data);
+      ?>
+    </p>
+  </body>
+</html>
