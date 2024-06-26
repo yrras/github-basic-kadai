f50eaf0c177674144616ec03e255399e3c37c753
yrras <161315617+yrras@users.noreply.github.com>
2024-06-26 21:55:07 +0900

    課題

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+// HTMLドキュメントが読み込み完了した際
+$(window).on("load", () => {
+  console.log('loadイベントが発生しました');
+});
+
+// 画面スクロールをした際
+$(window).on("scroll", () => {
+  console.log('scrollイベントが発生しました');
+});
